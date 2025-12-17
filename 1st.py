17196ae3fe31f80b47823ef671e379e772b821ef
abhi6754 <90463555+abhi6754@users.noreply.github.com>
2025-12-17 13:36:41 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter the 1st number: "))
+num2=int(input("Enter the 2nd number: "))
+
+print("Addition: ", num1 + num2)
+print("Subtraction: ", num1 - num2)
+print("Multiplication: ", num1 * num2)
+print("Divison: ", num1/num2)
