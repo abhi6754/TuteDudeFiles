1354017da1d318f57c641eb57d74a14eb31451c1
abhi6754 <90463555+abhi6754@users.noreply.github.com>
2025-12-25 22:53:23 +0530

    Update 5th.py

--- 5th.py
+++ 5th.py
@@ -1,9 +1,10 @@
 def factorial(n):
-    if n == 0:
+    if n == 1:
         return 1
     else:
         return n * factorial(n-1)
 
 num=int(input("Enter a number:"))
-print(factorial(num))
+res=factorial(num)
+print(f"The factorial of {num} is: {res}")
 
